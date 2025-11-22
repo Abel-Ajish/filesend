a061eb511f574bee4e511faabbee1b09f327a7c5
A-Alphabet <aaronajish2024@gmail.com>
2025-11-22 10:32:03 +0300

    update 11.1.3 - feat: Add Vercel Speed Insights integration to the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
